87e31589a1631969b02f7bf59cff74c4ba3d824a
kassuro <contact@kassuro.de>
2018-04-29 22:46:07 +0200

    Adding root("/") route functions

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,11 +1,14 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
+const Quote = require('../models/Quote');
 
-/* GET home page. */
-router.get('/', function (req, res) {
-    res.json({
-        title: 'Express'
-    });
+/*
+*   route '/'
+*   return a random quote
+*/
+router.get('/', async (req, res) => {
+    let quote = await Quote.query().select().orderByRaw('RAND()').limit(1).first();
+    res.json(quote);
 });
 
 module.exports = router;
